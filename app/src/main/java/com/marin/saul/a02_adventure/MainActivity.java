ac15f73639522f4c01e0b74509c3ba44a491bd40
Saul Marin <sau7.mar1n@gmail.com>
2017-01-13 10:47:18 +0100

    Added TakeActivity

--- app/src/main/java/com/marin/saul/a02_adventure/MainActivity.java
+++ app/src/main/java/com/marin/saul/a02_adventure/MainActivity.java
@@ -85,7 +85,8 @@ public class MainActivity extends AppCompatActivity {
         takeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                InventoryManager.take(currentRoom,inventory,"Mapa");
+                Intent i = new Intent(MainActivity.this, TakeActivity.class);
+                startActivity(i);
             }
         });
         dropButton.setOnClickListener(new View.OnClickListener() {
@@ -175,8 +176,12 @@ public class MainActivity extends AppCompatActivity {
         roomText = roomText + currentRoom.getDescription() + "\n";
 
         LinkedList<Item> roomItems = currentRoom.getItems();
-        for (Item item: roomItems){
-            roomText = roomText + item.getName()+ "\n";
+        if ( roomItems == null){
+            roomText = roomText + "No hay nada en la sala...";
+        }else {
+            for (Item item : roomItems) {
+                roomText = roomText + item.getName() + "\n";
+            }
         }
 
         roomDescription.setText(roomText);
