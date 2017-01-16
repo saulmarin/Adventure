34422a67688e2dfa948b02a65c41fd3791a6efa7
Saul Marin <sau7.mar1n@gmail.com>
2017-01-16 09:44:29 +0100

    Created Constants.class

--- app/src/main/java/com/marin/saul/a02_adventure/DropActivity.java
+++ app/src/main/java/com/marin/saul/a02_adventure/DropActivity.java
@@ -1,8 +1,11 @@
 package com.marin.saul.a02_adventure;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+
+import com.marin.saul.a02_adventure.model.Inventory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,11 +14,18 @@ public class DropActivity extends AppCompatActivity {
 
     @BindView(R.id.activity_drop_item_list) ListView itemList;
 
+    Inventory inventory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drop);
 
         ButterKnife.bind(this);
+
+        Intent i  = getIntent();
+        inventory = (Inventory) i.getSerializableExtra("KEY_INVENTORY");
+
+
     }
 }
---
+++ app/src/main/java/com/marin/saul/a02_adventure/util/Constants.java
@@ -0,0 +1,8 @@
+package com.marin.saul.a02_adventure.util;
+
+/**
+ * Created by usuario on 16/01/2017.
+ */
+
+public class Constants {
+}
