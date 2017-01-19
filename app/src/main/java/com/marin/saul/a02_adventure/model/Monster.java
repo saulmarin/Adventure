b43204d758537de6e97e967b61cac0f2b30a097c
saulmarin <sau7.mar1n@gmail.com>
2017-01-19 15:54:18 +0100

    Created Monster.java with: String name. String description. String imageUrl

---
+++ app/src/main/java/com/marin/saul/a02_adventure/model/Monster.java
@@ -0,0 +1,37 @@
+package com.marin.saul.a02_adventure.model;
+
+/**
+ * Created by g7-1105ss on 19/01/2017.
+ */
+
+public class Monster {
+
+    private String name;
+    private String description;
+    private String imageUrl;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
