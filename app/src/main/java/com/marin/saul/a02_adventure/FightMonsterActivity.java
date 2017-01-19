d54d2dc23e2df8cf4b3505ffeacd72fc7bd33b59
saulmarin <sau7.mar1n@gmail.com>
2017-01-19 16:13:10 +0100

    Created FightMonsterActivity

---
+++ app/src/main/java/com/marin/saul/a02_adventure/FightMonsterActivity.java
@@ -0,0 +1,13 @@
+package com.marin.saul.a02_adventure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FightMonsterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight_monster);
+    }
+}
