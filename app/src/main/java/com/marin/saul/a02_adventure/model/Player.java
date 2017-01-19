987d7ee5e9e6f56d1fb221fdc052f69036ca284c
saulmarin <sau7.mar1n@gmail.com>
2017-01-19 17:14:25 +0100

    Created Player.java. Added player on MainActivity.

---
+++ app/src/main/java/com/marin/saul/a02_adventure/model/Player.java
@@ -0,0 +1,23 @@
+package com.marin.saul.a02_adventure.model;
+
+import java.io.Serializable;
+import java.util.Random;
+
+
+public class Player implements Serializable{
+
+    private int healthPoints = 100;
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int attack(){
+        Random random = new Random();
+        return random.nextInt(50);
+    }
+}
