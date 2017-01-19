8be3c89ae5d913173c9a1c70f0bc4be8153c2717
saulmarin <sau7.mar1n@gmail.com>
2017-01-19 17:03:20 +0100

    FightMonsterActivity shows monster properties on screen

--- app/src/main/java/com/marin/saul/a02_adventure/FightMonsterActivity.java
+++ app/src/main/java/com/marin/saul/a02_adventure/FightMonsterActivity.java
@@ -1,13 +1,42 @@
 package com.marin.saul.a02_adventure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.marin.saul.a02_adventure.model.Monster;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FightMonsterActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_fight_monster_monster_image)ImageView monsterImage;
+    @BindView(R.id.activity_fight_monster_monster_name)TextView monsterName;
+    @BindView(R.id.activity_fight_monster_monster_description)TextView monsterDescription;
+    @BindView(R.id.activity_fight_monster_monster_life)TextView monsterLife;
+    @BindView(R.id.activity_fight_monster_player_life)TextView playerLife;
+    @BindView(R.id.activity_fight_monster_fight_button)Button fightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fight_monster);
+        ButterKnife.bind(this);
+
+        Intent i = getIntent();
+        Monster monster = (Monster) i.getSerializableExtra("monster");
+        monsterName.setText(monster.getName());
+        monsterDescription.setText(monster.getDescription());
+        Picasso.with(this).load(monster.getImageUrl()).into(monsterImage);
+
+
+
+
     }
 }
